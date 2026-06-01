bf1b399b143f613736a2cfc539f94772efe2b95f
nguyenvu512 <vu5122004@gmail.com>
2026-06-01 23:22:40 +0700

    [TranNguyenVu_22653141] add order service

--- order-service/src/main/java/com/example/orderservice/enums/PaymentStatus.java
+++ order-service/src/main/java/com/example/orderservice/enums/PaymentStatus.java
@@ -4,6 +4,8 @@ public enum PaymentStatus {
     PENDING("Pending"),
     DIRECT_PAYMENT("Direct Payment"),
     SUCCESSFUL_PAYMENT_WITH_VNPAY("Successful payment with vnpay"),
+    FAILED_PAYMENT_WITH_VNPAY("Failed payment with vnpay"),
+    VN_PAY("VN Pay")
     ;
 
     PaymentStatus(String name) {
